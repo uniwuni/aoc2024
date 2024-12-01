2fbd149238c69b255c9b61dc7aaf573707c9236d
Uni Marx <uniwuni@protonmail.com>
2024-12-01 12:47:27 +0100

    refactor to make it work on example lmao

--- src/day1.rs
+++ src/day1.rs
@@ -1,6 +1,24 @@
 use std::io::{self, BufRead};
 use std::str::FromStr;
-use std::collections::HashSet;
+use std::collections::HashMap;
+
+fn compute<A>(parsed: A) -> (u32, u32)
+  where A: Iterator<Item = (u32, u32)> {
+    /* day 1 */
+    let (mut left, mut right): (Vec<_>, Vec<_>) = parsed.unzip();
+    left.sort();
+    right.sort();
+    
+    let result1: u32 = left.iter().zip(right.iter()).map(|(&a,&b)| a.abs_diff(b)).sum();
+
+
+    /* day 2 */
+    let left_map: HashMap<&u32,u32> = left.iter().fold(HashMap::new(), |mut acc, n|
+        {*acc.entry(n).or_insert(0) += 1;
+        acc});
+    let result2: u32 = right.into_iter().map(|n| n * left_map.get(&n).unwrap_or(&0)).sum();
+    (result1, result2)
+}
 fn main() {
     let stdin = io::stdin();
     let parsed = stdin.lock().lines().map(|a| { let a = a.expect("where's my line");
@@ -9,17 +27,25 @@ fn main() {
                                                 let s2 = a.next().expect("where's my word");
                                                 (u32::from_str(s1).expect("where's my int"),
                                                  u32::from_str(s2).expect("where's my int"))});
-    
-    /* day 1 */
-    let (mut left, mut right): (Vec<_>, Vec<_>) = parsed.unzip();
-    left.sort();
-    right.sort();
-    
-    let result1: u32 = left.iter().zip(right.iter()).map(|(&a,&b)| a.abs_diff(b)).sum();
-    println!("{}", result1);
+    use std::time::Instant;
+    let now = Instant::now();                                            
 
-    /* day 2 */
-    let left_set: HashSet<&u32> = left.iter().collect();
-    let result2: u32 = right.into_iter().filter(|a| left_set.contains(a)).sum();
-    println!("{}", result2);
+    let (r1, r2) = compute(parsed);
+    let elapsed = now.elapsed();
+    println!("{} {}", r1, r2);
+
+    println!("Elapsed: {:.2?}", elapsed);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        // avoid parsing issues
+        let data = vec![(3,4),(4,3),(2,5),(1,3),(3,9),(3,3)];
+        assert_eq!(compute(data.into_iter()), (11,31));
+    }
 }
